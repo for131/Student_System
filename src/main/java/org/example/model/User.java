8b1c344f3175a8f92530ff4c455412cd12f9c386
tsy <tsytoseek@gmail.com>
2026-03-01 18:23:15 +0800

    写了user的基础javabean

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -5,4 +5,60 @@ import java.io.Serializable;
 public abstract class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private String username;
+    private String password;
+    private String name;
+    private String phone;
+    private String role;
+
+    public User(){
+    }
+
+    public User(String username, String role, String password, String name, String phone) {
+        this.username = username;
+        this.role = role;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
