6e008c4623f68e92343d81dfe126cc7107da0fe7
Nonono <1186692428@qq.com>
2026-03-02 16:28:32 +0800

    model

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -1,4 +1,24 @@
 package org.example.service;
 
+import org.example.dao.UserDao;
+import org.example.model.User;
+
+import java.util.List;
+
 public class UserService {
+    private UserDao userdao = new UserDao();
+    public static User currentUser;
+    public boolean login(String name,String password){
+        List<User> users = userdao.findLits(u -> u.getUsername().equals(name));
+        if(!users.isEmpty()){
+            User user = users.get(0);
+            if(user.getPassword() == password){
+                currentUser = user;
+                return true;
+            }
+
+        }
+        return false;
+    }
+
 }
