ce572476732c7e48376d791e5e4870d804a6e0d3
Nonono <1186692428@qq.com>
2026-03-02 10:55:07 +0800

    model

--- src/main/java/org/example/model/Admin.java
+++ src/main/java/org/example/model/Admin.java
@@ -3,8 +3,9 @@ package org.example.model;
 public class Admin extends User {
     public Admin(){
         super();
+        this.setRole("Admin");
     }
     public Admin(String username, String role, String password, String name, String phone){
-        super(username, role, password, name, phone);
+        super(username, "Admin", password, name, phone);
     }
 }
